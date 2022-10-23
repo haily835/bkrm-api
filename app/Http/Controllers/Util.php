13dcb9f6754a9465cf9dcd8729465ef47cc3ea94
haily835 <lyquochai835@gmail.com>
2022-10-23 13:12:13 +0700

    gitignore, folder structure

--- app/Http/Controllers/Util.php
+++ app/Http/Controllers/Util.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Storage;
 
 class Util {
     static function saveImage($image, $directory) {
-        $imagePath = $image->store('uploaded_images/' . $directory, 'public');
+        $imagePath = $image->store('uploaded-images/' . $directory, 'public');
         $url = Storage::url($imagePath);
         return env('APP_URL') . $url;
     }
